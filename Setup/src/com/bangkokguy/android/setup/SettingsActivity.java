ae6302b800e55b5efe13c87f5bb84a20c3786d1d
bangkokguy <basixonia@freemail.hu>
2013-07-02 11:50:22 +0200

    third commit

--- Setup/src/com/bangkokguy/android/setup/SettingsActivity.java
+++ Setup/src/com/bangkokguy/android/setup/SettingsActivity.java
@@ -1,7 +1,17 @@
 package com.bangkokguy.android.setup;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.bangkokguy.android.setup.faszom1;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 
@@ -25,5 +35,22 @@ public class SettingsActivity extends Activity {
       getMenuInflater().inflate(R.menu.main, menu);
       return true;
    }
+   Date date = null;
+   
+   public void StartService() {
+      String startDate = "20-6-2013 07:00";
+      SimpleDateFormat formatter = new SimpleDateFormat("d-M-yyyy hh:mm");
+      try {
+         date = formatter.parse(startDate);
+         }
+      catch (ParseException e) {Log.d("my_alarm", "date parse error");}
+      long millis = date.getTime();
+      
+      // prepare and fire the alarm manager; 1*1000*60*5=five minutes
+      Intent intent = new Intent(this, faszom1.class);
+      PendingIntent pintent = PendingIntent.getService(this, 0, intent, 0);
+      AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+      alarm.setRepeating(AlarmManager.RTC_WAKEUP, millis, 1*1000*60*5, pintent); 
+      }
 
 }
--- Setup/src/com/bangkokguy/android/setup/faszom1.java
+++ Setup/src/com/bangkokguy/android/setup/faszom1.java
@@ -1,6 +1,5 @@
 package com.bangkokguy.android.setup;
 
-import android.R;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
